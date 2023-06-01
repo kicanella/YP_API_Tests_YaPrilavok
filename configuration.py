bb03b8eaa65713025cc5c963cd22104faf333113
kicanella <qaqatest1@ya.ru>
2023-06-01 20:35:56 +0300

    Initial commit

---
+++ configuration.py
@@ -0,0 +1,3 @@
+# Здесь хранится сгенерированный URL Прилавка и путь до документации:
+URL_SERVICE = "https://14d2dcf6-43d0-4edd-824c-96d0dd8040c5.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
---
+++ sender_stand_request.py
@@ -0,0 +1,7 @@
+import configuration
+import requests
+def get_docs():
+    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+
+response = get_docs()
+print(response.status_code)
