d2999f1ded91abb44a17c1145553c43e7bd99fee
kicanella <qaqatest1@ya.ru>
2023-06-01 22:32:57 +0300

    Добавлен новый тест

--- sender_stand_request.py
+++ sender_stand_request.py
@@ -1,7 +1,16 @@
 import configuration
 import requests
-def get_docs():
-    return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
 
-response = get_docs()
+# Задание 1:
+# def get_docs():
+# return requests.get(configuration.URL_SERVICE + configuration.DOC_PATH)
+# response = get_docs()
+# print(response.status_code)
+
+# Задание 2:
+def get_logs():
+    return requests.get(configuration.URL_SERVICE + configuration.LOG_MAIN_PATH)
+
+response = get_logs()
 print(response.status_code)
+print(response.headers)
